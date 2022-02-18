a087f42373b67a669835403757a0716d9b515d87
donngchao <1559273720@qq.com>
2022-02-18 22:38:58 +0800

    Create HelloRun.vue

---
+++ HelloRun.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="list-rendering">
+    <ol>
+      <li v-for="todo in todos" v-bind:key="todo">
+        {{ todo.text }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloRun",
+  data() {
+    return {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
