366769a61a615d2f7388c1edda650cb2d25a4852
donngchao <1559273720@qq.com>
2022-05-06 07:41:00 +0800

    Create App7.vue

---
+++ vue_basics/App7.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      awesome: true
+    }
+  },
+  methods: {
+    toggle() {
+      // toggle between true and false
+      this.awesome = !this.awesome
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="toggle">toggle</button>
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else>Oh no 😢</h1>
+</template>
