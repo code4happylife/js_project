4c2ed753419b917b5991339c3bfd6e3437563d31
donngchao <1559273720@qq.com>
2022-05-06 07:15:58 +0800

    Create App2.vue

---
+++ vue_basics/App2.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  // component options
+  data(){
+    return{
+      message:"Hello World!",
+      message2:"This is so cool!"
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Hello World!</h1>
+  <h1>{{message}}</h1>
+  <h1>
+    {{message2}}
+  </h1>
+</template>
