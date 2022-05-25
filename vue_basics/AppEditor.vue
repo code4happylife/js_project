8865293f024985e2d6a48ac7518267629368b330
donngchao <1559273720@qq.com>
2022-05-26 07:49:07 +0800

    Create AppEditor.vue

---
+++ vue_basics/AppEditor.vue
@@ -0,0 +1,30 @@
+<template>
+  <Editor v-model="editData" lang='python' theme="monokai" height="400px"></Editor>
+
+</template>
+
+<script>
+import Editor from "@/components/Editor";
+export default {
+  name: 'App',
+  components: {
+    Editor
+  },
+  data(){
+    return{
+      editData:""
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
